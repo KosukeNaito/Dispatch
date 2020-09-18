538d79762e07ad5b3c415696fa4c97b1a53a595a
KosukeNaito <tonightoo7110@gmail.com>
2020-09-19 00:02:44 +0900

    add testSetContent

--- Tests/TagTest.php
+++ Tests/TagTest.php
@@ -7,12 +7,18 @@ class TagTest extends TestCase {
     public function testConstruct() {
 
         require 'HTMLBuilder/Tag.php';
-
         $tag = new Tag('tagName', 'testContent');
-
-        $this->assertEquals('<tagName>testContent</tagName>',$tag->getTagText());
+        $this->assertEquals('<tagName>testContent</tagName>', $tag->getTagText());
 
     }
+
+    public function testSetContent() {
+        $tag = new Tag('tagName', 'testContent');
+        $tag->setContent('overrideContent');
+        $this->assertEquals('<tagName>overrideContent</tagName>', $tag->getTagText());
+    }
+
+    
 
 }
 
