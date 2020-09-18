4f5ffdd657363c3dce4927b1e83cd623fc68a9ce
KosukeNaito <tonightoo7110@gmail.com>
2020-09-19 00:15:16 +0900

    add testWriteAttributes

--- Tests/TagTest.php
+++ Tests/TagTest.php
@@ -5,20 +5,31 @@ use PHPUnit\Framework\TestCase;
 class TagTest extends TestCase {
 
     public function testConstruct() {
-
         require 'HTMLBuilder/Tag.php';
+
         $tag = new Tag('tagName', 'testContent');
         $this->assertEquals('<tagName>testContent</tagName>', $tag->getTagText());
 
+        $tag = new Tag('emptyTag', '');
+        $this->assertEquals('<emptyTag></emptyTag>', $tag->getTagText());
     }
 
     public function testSetContent() {
+
         $tag = new Tag('tagName', 'testContent');
         $tag->setContent('overrideContent');
         $this->assertEquals('<tagName>overrideContent</tagName>', $tag->getTagText());
+
     }
 
-    
+    public function testWriteAttributes() {
+
+        $tag = new Tag('attTest', '');
+        $tag->addAttribute('class', 'testClass');
+        $this->assertEquals('<attTest class=\'testClass\'></attTest>', $tag->getTagText());
+
+
+    }
 
 }
 
